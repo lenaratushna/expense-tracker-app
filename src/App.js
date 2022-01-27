9f512538e65cc2b6922befb3e60e650daa765aba
Lena <olena.ratushna21@gmail.com>
2022-01-27 16:28:16 +0200

    Added 'Main' and 'Details' components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-function App() {
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Main from "./components/Main/Main";
+import Details from "./components/Details/Details";
+import useStyles from "./styles";
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      hello
-    </div>
+    <Grid
+      className={classes.grid}
+      container
+      alignItems="center"
+      justifyContent="center"
+      style={{ height: "100vh" }}
+    >
+      <Grid item xs={12} sm={6}>
+        <Main />
+      </Grid>
+      <Grid item container direction="column" spacing={2} xs={12} sm={5}>
+      <Grid item>
+        <Details title="Income"/>
+      </Grid>
+      <Grid item>
+      <Details title="Expense" />
+      </Grid> 
+      </Grid>
+
+    </Grid>
   );
-}
+};
 
 export default App;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Typography, Card, CardHeader, CardContent, Divider, Grid } from "@material-ui/core";
+
+import useStyles from "./styles";
+
+const Main = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" />
+      <CardContent>
+        <Typography variant="h5" align="center">
+          Total Balance $1
+        </Typography>
+        <Divider />
+
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/* <List /> */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
