887f13b702e5e87cd7cf6f81fca1c0e3224b087a
Lena <olena.ratushna21@gmail.com>
2022-01-31 13:14:03 +0200

    Added counter for total balance

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Typography, Card, CardHeader, CardContent, Divider, Grid } from '@material-ui/core';
 
 import useStyles from './styles';
 import Form from './Form/Form';
 import List from './List/List';
+import { ExpenseTrackerContext } from '../../context/context';
 
 const Main = () => {
   const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
 
   return (
     <Card className={classes.root}>
       <CardHeader title="Expense Tracker" />
       <CardContent>
         <Typography variant="h5" align="center">
-          Total Balance $1
+          Total Balance ${balance}
         </Typography>
         <Divider className={classes.divider}/>
         <Form />
